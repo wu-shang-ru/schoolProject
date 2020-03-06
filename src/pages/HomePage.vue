9df4d09644141b0c6e32490231a857000078ceab
Shang-Ru <best0049@gmail.com>
2020-03-06 16:54:47 +0800

    'VueCli'

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="HomePage">123</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
