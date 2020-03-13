00c340e1385b9bab0e51957bc02cabeb1310c1a3
Shang-Ru <best0049@gmail.com>
2020-03-13 14:23:16 +0800

    'Updata'

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="HomePage">123</div>
+  <div class="HomePage">
+    <div>123</div>
+    <HelloWorld></HelloWorld>
+  </div>
 </template>
 
 <script>
+import HelloWorld from "@/components/HelloWorld";
+
 export default {
-  name: 'HomePage'
-}
+  name: "HomePage",
+  components: {
+    HelloWorld
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
