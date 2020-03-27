5223541e5835053eab8a38efe0c5bab735586190
wu-shang-ru <[best0049@gmail.com]>
2020-03-27 23:27:19 +0800

    banner

---
+++ ucc_web/src/components/Banner.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="banner">
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in photoList" :key="item">
+        <img class="picture" :src="item.url" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "banner",
+  data() {
+    return {
+      photoList: [
+        {
+          url: require("@/assets/banner/1.jpg")
+        },
+        {
+          url: require("@/assets/banner/2.jpg")
+        },
+        {
+          url: require("@/assets/banner/3.jpg")
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+
+.picture {
+  width: 100%;
+  height: 100%;
+}
+
+.banner {
+  margin: 10px 180px;
+}
+</style>
