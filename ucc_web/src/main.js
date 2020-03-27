3c4146506a779093ca0855046a83721704fe8af2
wu-shang-ru <[best0049@gmail.com]>
2020-03-27 23:28:34 +0800

    codingStyle

--- ucc_web/src/main.js
+++ ucc_web/src/main.js
@@ -6,20 +6,20 @@ import 'bootstrap'; // Import js file
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import css file
 import BootstrapVue from 'bootstrap-vue'
 
-// 為了加入fontawesome icon 所import
+// fontawesomeIcon
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faComment, faUserCircle, faUserFriends, faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faComment, faUserCircle, faUserFriends, faBars, faSearch)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-// 結束
 
 //ElementUI
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import '../theme/index.css';
 
 Vue.use(ElementUI);
 Vue.use(BootstrapVue);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+library.add(faComment, faUserCircle, faUserFriends, faBars, faSearch)
 
 Vue.config.productionTip = false;
 
