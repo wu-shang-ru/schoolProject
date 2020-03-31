7c99533b7a31e59649f0ce3fb787bebbcced29bb
EricHsueh88 <erichsueh880605@gmail.com>
2020-03-31 19:23:58 +0800

    03/31

--- ucc_web/src/components/Menubar.vue
+++ ucc_web/src/components/Menubar.vue
@@ -1,45 +1,59 @@
 <template>
   <div class="Menubar">
-    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-      <el-radio-button :label="false">展开</el-radio-button>
-      <el-radio-button :label="true">收起</el-radio-button>
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;margin-top:20px;">
+      <el-radio-button :label="false" v-show="isOpen" @click="openClose">展开</el-radio-button>
+      <el-radio-button :label="true" v-show="!isOpen" @click="openClose">收起</el-radio-button>
     </el-radio-group>
     <el-menu
-      default-active="1-4-1"
+      default-active="1"
       class="el-menu-vertical-demo"
       @open="handleOpen"
       @close="handleClose"
       :collapse="isCollapse"
     >
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
-        </template>
-        <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+      <el-menu-item index="1">
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646029"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
       </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
+      <el-menu-item index="2">
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646029"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
       </el-menu-item>
       <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646029"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
       </el-menu-item>
     </el-menu>
   </div>
@@ -49,7 +63,8 @@
 export default {
   data() {
     return {
-      isCollapse: true
+      isCollapse: true,
+      isOpen: true
     };
   },
   methods: {
@@ -58,6 +73,9 @@ export default {
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    },
+    openClose() {
+      this.isOpen = !this.isOpen;
     }
   }
 };
@@ -71,5 +89,8 @@ export default {
 }
 .Menubar {
   position: absolute;
+}
+.menuIcon {
+  border-radius: 50%;
 }
 </style>
