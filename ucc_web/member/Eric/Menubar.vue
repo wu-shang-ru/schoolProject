3e3cbb665a09172aa04950f454e350390e2bb901
EricHsueh88 <erichsueh880605@gmail.com>
2020-03-31 22:54:23 +0800

    Menubar-finish-in-03-31

--- ucc_web/member/Eric/Menubar.vue
+++ ucc_web/member/Eric/Menubar.vue
@@ -1,69 +1,101 @@
 <template>
   <div class="Menubar">
-    <el-col :span="4">
-      <h5>找社團? 找活動?</h5>
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>我的社團</span>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;margin-top:20px;">
+      <div @click="openClose">
+        <el-radio-button :label="false" v-if="isOpen">展开</el-radio-button>
+      </div>
+      <div @click="openClose">
+        <el-radio-button :label="true" v-if="!isOpen">收起</el-radio-button>
+      </div>
+    </el-radio-group>
+    <el-menu
+      default-active="1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+    >
+      <el-menu-item index="1">
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646029"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646029"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <a href="https://google.com">
+          <img
+            class="menuIcon"
+            src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646029"
+            width="35px"
+            height="35px"
+          />
+        </a>
+        <span slot="title">社團名稱</span>
+      </el-menu-item>
+    </el-menu>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Menubar",
+  data() {
+    return {
+      isCollapse: true,
+      isOpen: true
+    };
+  },
   methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      }
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    openClose() {
+      // this.$emit("click".fn);
+      this.isOpen = !this.isOpen;
     }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.Menubar{
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+.Menubar {
   position: absolute;
-  top: 60px;
-  width:100%;
+}
+.menuIcon {
+  border-radius: 50%;
 }
 </style>
