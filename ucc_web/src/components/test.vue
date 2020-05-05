901b749827b868d0ac9b96feb18d7ae4ffa10656
EricHsueh88 <erichsueh880605@gmail.com>
2020-05-05 22:52:45 +0800

    using css grid to display the chatPage in 0505

--- ucc_web/src/components/test.vue
+++ ucc_web/src/components/test.vue
@@ -1,254 +1,69 @@
 <template>
-  <div class="Menubar-new">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-    >
-      <input type="checkbox" id="check" />
-      <el-menu-item index="1">
-        <router-link to="/">
-          <img class="logo" src="../../member/Eric/img/UCC Classic.jpg" />
-          <span id="fl">
-            <h3>University Club Center</h3>
-          </span>
-        </router-link>
-      </el-menu-item>
-      <el-menu-item id="inputArea">
-        <el-input v-model="search" @focus="searchOnfocus" @blur="searchOnblur" clearable>
-          <el-button slot="append" icon="el-icon-search"></el-button>
-        </el-input>
-      </el-menu-item>
-
-      <div class="rightBtnGroup">
-        <router-link to="/chat" style="text-decoration:none;">
-          <el-menu-item index="4" class="rightBtn">
-            <i class="el-icon-chat-line-square" size="medium"></i>
-            <span class="navFont">Chat</span>
-          </el-menu-item>
-        </router-link>
-
-        <el-submenu index="5" href="#" class="rightBtn" id="followersBtn">
-          <template slot="title">
-            <font-awesome-icon
-              icon="user-friends"
-              size="lg"
-              style="color:#A9A9A9;margin-right:8px;"
-            />
-            <span id="followers" style="font-size:16px;">Followers</span>
-          </template>
-          <el-menu-item class="rightBtn" index="5-1" href="#" id="dropDownBtn">追蹤者</el-menu-item>
-          <el-menu-item class="rightBtn" index="5-2" href="#" id="dropDownBtn">追蹤中的社團</el-menu-item>
-        </el-submenu>
-
-        <!-- 登入狀態改變時會有不同的項目出現 -->
-        <!-- 未登入 -->
-        <el-menu-item
-          index="6"
-          href="#"
-          class="rightBtn"
-          data-toggle="modal"
-          data-target="#Login"
-          v-if="loginState === false"
-        >
-          <i class="el-icon-user-solid" style="color:#A9A9A9"></i>
-          <span class="navFont">Login</span>
-        </el-menu-item>
-        <!-- 已登入 -->
-        <el-submenu index="6" href="#" class="rightBtn" v-if="loginState === true">
-          <template slot="title" class="rightBtn">
-            <i class="el-icon-user-solid" style="color:#A9A9A9"></i>
-            <span class="navFont">User</span>
-          </template>
-          <router-link to="/user/info" style="text-decoration:none;">
-            <el-menu-item class="rightBtn" index="6-1" href="#">個人資料</el-menu-item>
-          </router-link>
-          <el-menu-item class="rightBtn" index="6-2" href="#" id="dropDownBtn">發佈新活動</el-menu-item>
-          <el-menu-item class="rightBtn" index="6-3" @click.native="logout" id="dropDownBtn">登出</el-menu-item>
-        </el-submenu>
+  <div class="Chat">
+    <Navbar></Navbar>
+    <Menubar></Menubar>
+    <div class="container">
+      <div class="msg-header">msg-header</div>
+      <div class="chatArea">
+        chatArea
+        <div class="inputArea">inputArea</div>
       </div>
-    </el-menu>
-    <label for="check">
-      <font-awesome-icon class="checkBtn" icon="bars" size="lg" style="color:#A9A9A9" />
-    </label>
-    <Login></Login>
-    <register></register>
+      <div class="searchFriend">
+        searchFriend
+        <div class="friendList">friendList</div>
+      </div>
+    </div>
   </div>
-</template>  
+</template>
+
 <script>
-import { mapActions } from "vuex";
-import { authenticated } from "@/utils/AuthStore";
-import Login from "@/components/Login";
-import register from "@/components/Register";
-
+import Menubar from "@/components/Menubar";
+import Navbar from "@/components/Navbar";
 export default {
-  name: "Navbar",
-
   data() {
     return {
-      email: "",
-      password: "",
-      search: "搜尋社團/活動",
-      token: {
-        tokenType: "",
-        accessToken: ""
-      },
-      activeIndex: "1",
-      activeIndex2: "1",
-      loginState: ""
+      input: ""
     };
   },
-
+  name: "Chat",
   components: {
-    Login,
-    register
-  },
-
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    // search的使用者友善
-    searchOnfocus() {
-      if (this.search === "搜尋社團/活動") {
-        this.search = "";
-      }
-    },
-    searchOnblur() {
-      if (this.search === "") {
-        this.search = "搜尋社團/活動";
-      }
-    },
-
-    ...mapActions({
-      logout: "auth/logout"
-    })
-  },
-
-  mounted() {
-    if (authenticated()) {
-      this.loginState = true;
-    } else {
-      this.loginState = false;
-    }
+    Navbar,
+    Menubar
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.rightBtnGroup {
-  display: flex;
-  justify-content: flex-end;
+.container {
+  max-width: 800px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 20px;
 }
-.Icon {
-  border: 0px;
+.msg-header {
+  background-color: #ddd;
+  grid-column: 1/4;
+  height: 10vh;
 }
-.logo {
-  height: 40px;
-  width: 70px;
-  margin: 0 13px 5px 0;
+.chatArea {
+  background-color: #eee;
+  grid-column: 1/3;
+  height: 70vh;
+  display: grid;
+  grid-template-rows: repeat(8, 1fr);
 }
-#fl {
-  margin-top: 10px;
-  float: right;
+.searchFriend {
+  background-color: #ccc;
+  display: grid;
+  grid-template-rows: repeat(8, 1fr);
 }
-#followersBtn {
-  width: 80px;
+.inputArea {
+  background-color: #aaa;
+  grid-row: 8/9;
 }
-#followers {
-  color: white;
-  display: none;
-}
-#check {
-  display: none;
-}
-.checkBtn {
-  display: none;
-  cursor: pointer;
-  position: fixed;
-  top: 20px;
-  right: 30px;
-}
-.navFont {
-  display: none;
-}
-#dropDownBtn:hover {
-  background-color: #fff5e4;
-}
-@media screen and (max-width: 910px) {
-  .rightBtnGroup {
-    position: fixed;
-    width: 200px;
-    height: 168px;
-    top: 60px;
-    flex-direction: column;
-    right: 0;
-    text-align: center;
-    background-color: rgb(255, 180, 94);
-    right: -200px;
-    transition: all 0.3s ease;
-  }
-  .rightBtn {
-    display: block;
-    font-size: 16px;
-    color: white;
-    text-decoration: none;
-  }
-  .rightBtn:hover {
-    color: black;
-  }
-  .checkBtn {
-    display: block;
-  }
-  .navFont {
-    display: inline;
-  }
-  #check:checked ~ .rightBtnGroup {
-    right: 0;
-  }
-  #followers:hover {
-    color: #000;
-  }
-  #followers {
-    display: inline;
-  }
-  #followersBtn {
-    width: 100%;
-  }
-  font-awesome-icon {
-    color: white;
-  }
-}
-@media screen and (max-width: 767px) {
-  #inputArea {
-    position: fixed;
-    top: 60px;
-    left: 0;
-    width: 250px;
-  }
-}
-@media screen and (max-width: 485px) {
-  .checkBtn {
-    position: fixed;
-    top: 80px;
-    right: 10px;
-  }
-  .rightBtnGroup {
-    top: 120px;
-    width: 180px;
-  }
-}
-@media screen and (max-width: 410px) {
-  #fl h3 {
-    font-size: 1.5rem;
-    margin-top: 5px;
-  }
-  .logo {
-    height: 30px;
-    width: 55px;
-  }
+.friendList {
+  background-color: #bbb;
+  grid-row: 2/9;
 }
 </style>
