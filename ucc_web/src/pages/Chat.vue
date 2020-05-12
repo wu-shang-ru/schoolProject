2dc6e7da70d35929f4cb7193065d75a81ac98da3
EricHsueh88 <erichsueh880605@gmail.com>
2020-05-12 12:02:03 +0800

    chatPageUI finish

--- ucc_web/src/pages/Chat.vue
+++ ucc_web/src/pages/Chat.vue
@@ -15,20 +15,86 @@
           </a>
         </div>
       </div>
-      <div class="chatArea">
-        chatArea
-        <div class="inputArea">
-          <div class="inputArea-container">
-            <font-awesome-icon icon="paperclip" size="lg" style="color:#fff;margin-right:1rem;" />
-            <el-input v-model="input2" placeholder="请输入内容"></el-input>
+      <div class="chatArea">chatArea</div>
+      <div class="friendList">
+        <div class="conversationList">
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">對話內容，對話過長會出事</small>
+          </div>
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">I Love U js</small>
+          </div>
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">I Love U js</small>
+          </div>
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">I Love U js</small>
+          </div>
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">I Love U js</small>
+          </div>
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">I Love U js</small>
+          </div>
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">I Love U js</small>
+          </div>
+          <div class="conversation">
+            <img
+              src="http://ntcbadm1.ntub.edu.tw/Inc/ShowIndexStdImg.ashx?dataPic=10646027"
+              alt="avatar"
+            />
+            <div class="conversation-name">Roger Wu</div>
+            <small class="conversation-content">I Love U js</small>
           </div>
         </div>
       </div>
-      <div class="friendList">
-        <div class="conversation">sodifjosidjf</div>
-        <div class="searchFriend">
-          <el-input placeholder="搜尋朋友" prefix-icon="el-icon-search" v-model="input1" clearable></el-input>
+      <div class="inputArea">
+        <div class="inputArea-container">
+          <label style="margin-bottom:-3px;margin-right:1rem;">
+            <input type="file" style="display:none;" />
+            <font-awesome-icon icon="paperclip" size="lg" style="color:#fff;" />
+          </label>
+          <el-input v-model="input2" placeholder="请输入内容"></el-input>
         </div>
+      </div>
+      <div class="searchFriend">
+        <el-input placeholder="搜尋朋友" prefix-icon="el-icon-search" v-model="input1" clearable></el-input>
       </div>
     </div>
   </div>
@@ -56,40 +122,53 @@ export default {
 <style scoped>
 .container {
   max-width: 800px;
+  max-height: 100vh;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(8, 1fr);
   margin-top: 20px;
   margin-left: 20%;
   box-sizing: border-box;
+  padding: 0;
 }
 .msg-header {
   background-color: rgb(255, 148, 60);
   grid-column: 1/4;
   height: 10vh;
+  grid-row: 1/2;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+  border-bottom: white 1px solid;
 }
 .chatArea {
   background-color: #eee;
   grid-column: 1/3;
-  height: 70vh;
-  display: grid;
-  grid-template-rows: repeat(8, 1fr);
+  grid-row: 2/8;
+  border-left: rgb(255, 148, 60) 2px solid;
 }
 .friendList {
   background-color: #ccc;
-  display: grid;
-  grid-template-rows: repeat(8, 1fr);
+  grid-row: 2/8;
+  overflow-y: hidden;
+  max-height: 60vh;
+}
+.friendList:hover {
+  overflow-y: scroll;
 }
 .inputArea {
-  background-color: #aaa;
+  background-color: rgb(255, 148, 60);
   grid-row: 8/9;
+  grid-column: 1/3;
+  border-bottom-left-radius: 10px;
+  border-right: #fff 1px solid;
 }
 .searchFriend {
-  background-color: #bbb;
+  background-color: rgb(255, 148, 60);
   grid-row: 8/9;
   padding: 0 1rem 0 1rem;
   display: grid;
   align-items: center;
-  grid-area: .searchFriend;
+  border-bottom-right-radius: 10px;
 }
 .searchFriend el-input {
   border-radius: 30px;
@@ -123,8 +202,58 @@ export default {
   height: 100%;
   padding: 0 1rem 0 1rem;
 }
-.conversation {
-  padding: 20px;
-  background-color: #fff;
+.inputArea-container label {
+  transition: all 0.8s ease;
+  cursor: pointer;
 }
+.inputArea-container label:hover {
+  transform: scale(1.3, 1.3);
+}
+.conversation {
+  padding: 10px;
+  background-color: rgb(255, 150, 80);
+  height: 80px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  border-bottom: white 1px solid;
+  color: white;
+  transition: all 0.3s ease;
+}
+.conversation:hover {
+  background: rgb(245, 168, 116);
+}
+.conversation img {
+  height: 45px;
+  width: 45px;
+  border-radius: 50%;
+}
+.conversation-name {
+  align-self: start;
+  margin: 5px 0 0 5px;
+  font-weight: bold;
+}
+.conversation-content {
+  margin: 10px 0 0 -73px;
+  overflow-x: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: #ddd;
+}
+::-webkit-scrollbar {
+  width: 7px;
+}
+::-webkit-scrollbar-track {
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  background-color: #f5f5f5;
+}
+::-webkit-scrollbar-thumb {
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+  background: #bbb;
+}
+/* ::-webkit-scrollbar:hover {
+  display: contents;
+} */
 </style>
